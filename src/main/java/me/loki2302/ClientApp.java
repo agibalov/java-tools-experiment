674ac78225248b37e5531c8be5f8cd51ef9a241c
loki2302 <loki2302@loki2302.me>
2013-07-19 22:11:09 +0400

    Initial version

---
+++ src/main/java/me/loki2302/ClientApp.java
@@ -0,0 +1,7 @@
+package me.loki2302;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        System.out.println("I am client");
+    }
+}
---
+++ src/main/java/me/loki2302/ServerApp.java
@@ -0,0 +1,7 @@
+package me.loki2302;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("I am server");
+    }
+}
