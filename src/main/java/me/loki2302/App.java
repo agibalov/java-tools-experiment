8de05d36619e721a4eec0b11ae706372a3d5749f
loki2302 <loki2302@loki2302.me>
2016-10-01 12:40:56 -0400

    Initial version

---
+++ src/main/java/me/loki2302/App.java
@@ -0,0 +1,33 @@
+package me.loki2302;
+
+public class App {
+    /**
+     * Application entry point.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO: make it more useful
+        System.out.printf("hello world: %d\n", add(2, 3));
+    }
+
+    /**
+     * Adds 2 numbers.
+     * @param a number A
+     * @param b number B
+     * @return sum of A and B
+     */
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    // TODO: how do I require min length for summary and tags?
+    /**
+     * a.
+     * @param a a
+     * @param b b
+     * @return r
+     */
+    private static int sub(int a, int b) {
+        return a + b;
+    }
+}
